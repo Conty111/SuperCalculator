65765e84f5a7e090127b57f1e8dca100b1d9b343
Conty111 <mitj88@gmail.com>
2024-04-16 19:14:17 +0300

    [update] little refactoring

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Прочитать JSON и найти количество агентов
-num_agents=$(jq '.agents | length' system_config.json)
-
-# Установить количество агентов в переменную среды
-export AGENTS_COUNT=$num_agents
-docker compose -f docker-compose.yml up -d
