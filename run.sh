11891608b976c3a43e2f039410aee1001dc56849
Conty111 <mitj88@gmail.com>
2024-04-17 12:33:05 +0300

    kafka partitions error

--- run.sh
+++ run.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 # Чтение количества агентов из JSON файла
-agents_count=$(jq '.agents | length' agents.json)
+agents_count=$(jq '.agents | length' system_config_docker.json)
 
 # Замена значения переменной AGENTS_COUNT в docker-compose файле
-export AGENTS_COUNT=$agents_count
-docker build -t svc-agent:latest DockerfileAgent .
+export AGENTS_COUNT=3
+docker build -t svc-agent:latest -f DockerfileAgent .
 # Запуск docker-compose (все компоненты, кроме agent)
-docker-compose up orkestrator -d
+docker-compose up -d kafka
 
-for ((i=0;i<agents_count;i++))
+for ((i=1;i<agents_count+1;i++))
 do
     docker run -d \
             --name agent$i \
+            --add-host=agent$i:127.0.0.1 \
             --restart on-failure \
-            --env AGENTS_COUNT=$agents_count \
-            --env-file enviroments/agent.env \
-            --env-file enviroments/kafka.env \
-            --env-file enviroments/.env \
-            --network svc-network \
+            --env-file=enviroments/agent.env \
+            --env-file=enviroments/kafka.env \
+            --env-file=enviroments/.env \
+            --network=supercalculator_calculator-network \
             svc-agent \
-            /app serve $i
+            /app serve ${i-1}
+#            yes
 done
+
+docker-compose up -d orkestrator
