48b189066dffed84e19d9fbc883edb6199367c15
Conty111 <mitj88@gmail.com>
2024-04-18 14:46:36 +0300

    [update & fix] logs and debug

--- stop.sh
+++ stop.sh
@@ -3,5 +3,4 @@
 # Остановка и удаление всех запущенных контейнеров
 docker stop $(docker ps -aq --filter "name=^/agent")
 docker rm $(docker ps -aq --filter "name=^/agent")
-docker rmi $(docker images -q --filter "name=svc-agent")
 docker-compose down
